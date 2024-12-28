e9aae1215abf8b19f0162b407fc356059f5cb6e7
Mobin Munir <mmobinbutt@gmail.com>
2024-12-29 01:00:33 +0500

    Started working on login business logic.

--- src/commonMain/kotlin/onlytrade/app/viewmodel/login/LoginViewModel.kt
+++ src/commonMain/kotlin/onlytrade/app/viewmodel/login/LoginViewModel.kt
@@ -1,14 +1,7 @@
 package onlytrade.app.viewmodel.login
 
 import androidx.lifecycle.ViewModel
-import onlytrade.app.viewmodel.login.usecase.EmailLoginUseCase
-import onlytrade.app.viewmodel.login.usecase.MobileLoginUseCase
 
-class LoginViewModel(
-    private val mobileLoginUseCase: MobileLoginUseCase,
-    private val emailLoginUseCase: EmailLoginUseCase
-) : ViewModel() {
+class LoginViewModel:ViewModel(){
 
-
-    fun doMobileLogin(phoneNumber: String) = mobileLoginUseCase(phoneNumber)
 }

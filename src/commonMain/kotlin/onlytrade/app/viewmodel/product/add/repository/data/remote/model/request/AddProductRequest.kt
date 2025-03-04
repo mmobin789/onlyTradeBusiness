3b048e691d47fe74072f3f95aff9cc6d80c8542c
Mobin Munir <mmobinbutt@gmail.com>
2025-03-05 01:45:24 +0500

    Working on product business logic.

--- src/commonMain/kotlin/onlytrade/app/viewmodel/product/add/repository/data/remote/model/request/AddProductRequest.kt
+++ src/commonMain/kotlin/onlytrade/app/viewmodel/product/add/repository/data/remote/model/request/AddProductRequest.kt
@@ -1,11 +1,9 @@
 package onlytrade.app.viewmodel.product.add.repository.data.remote.model.request
 
 
-import java.io.File
-
 data class AddProductRequest(
     val name: String,
     val desc: String,
     val price: Int,
-    val images: List<File>
+    val images: List<ByteArray>
 )

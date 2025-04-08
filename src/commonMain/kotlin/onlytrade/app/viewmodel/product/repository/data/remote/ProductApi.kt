3fdfb0d6fb077ff7d9eefe4add2a8008a7d994cd
Mobin Munir <mmobinbutt@gmail.com>
2025-04-08 13:36:33 +0500

    Feature: JWT Auth Implementation
    * Working on jwt token approach.
    
    * Updated client to use jwt instead of basic auth.

--- src/commonMain/kotlin/onlytrade/app/viewmodel/product/repository/data/remote/ProductApi.kt
+++ src/commonMain/kotlin/onlytrade/app/viewmodel/product/repository/data/remote/ProductApi.kt
@@ -3,14 +3,15 @@ package onlytrade.app.viewmodel.product.repository.data.remote
 import io.github.aakira.napier.Napier
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
-import io.ktor.client.request.basicAuth
 import io.ktor.client.request.forms.MultiPartFormDataContent
 import io.ktor.client.request.forms.formData
+import io.ktor.client.request.header
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.http.Headers
 import io.ktor.http.HttpHeaders
 import kotlinx.serialization.json.Json
+import onlytrade.app.viewmodel.login.repository.data.remote.LoginApi
 import onlytrade.app.viewmodel.product.add.repository.data.remote.request.AddProductRequest
 import onlytrade.app.viewmodel.product.add.repository.data.remote.response.AddProductResponse
 
@@ -19,7 +20,7 @@ class ProductApi(private val client: HttpClient) { //todo
     suspend fun addProduct(addProductRequest: AddProductRequest) =
         try {
             val httpResponse = client.post("https://onlytrade.co/product/add") {
-                basicAuth("test@ot.co", "test123") //todo
+                header(HttpHeaders.Authorization, "Bearer ${LoginApi.jwtToken}") //todo get token here from local storage.
 
                 setBody(MultiPartFormDataContent(formData {
                     append(

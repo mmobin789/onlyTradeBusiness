c386e90eff0f07f2696ecce1caf2923dcaa83ae6
Mobin Munir <mmobinbutt@gmail.com>
2025-04-13 21:48:44 +0500

    Working on 3rd party KV storage impl for KMP.

--- src/commonMain/kotlin/onlytrade/app/viewmodel/login/repository/LoginRepository.kt
+++ src/commonMain/kotlin/onlytrade/app/viewmodel/login/repository/LoginRepository.kt
@@ -1,10 +1,24 @@
 package onlytrade.app.viewmodel.login.repository
 
+import com.russhwolf.settings.Settings
+import kotlinx.serialization.json.Json
+import onlytrade.app.viewmodel.login.repository.data.LoginConst.JWT_TOKEN
+import onlytrade.app.viewmodel.login.repository.data.LoginConst.JWT_USER
 import onlytrade.app.viewmodel.login.repository.data.remote.LoginApi
 
-class LoginRepository(private val loginApi: LoginApi) {
+class LoginRepository(private val loginApi: LoginApi, private val settings: Settings) {
 
-    suspend fun loginWithPhone(mobileNo: String, pwd: String) = loginApi.loginByPhone(mobileNo, pwd)
+    suspend fun loginWithPhone(mobileNo: String, pwd: String) =
+        loginApi.loginByPhone(mobileNo, pwd)?.also {
+            it.jwtToken?.run {
+                settings.putString(JWT_TOKEN, this)
+            }
+            it.user?.run {
+                val user = Json.encodeToString(this)
+                settings.putString(JWT_USER, user)
+            }
+        }
+
 
     suspend fun loginWithEmail(email: String, pwd: String) = loginApi.loginByEmail(email, pwd)
 }
---
+++ src/commonMain/kotlin/onlytrade/app/viewmodel/login/repository/data/LoginConst.kt
@@ -0,0 +1,6 @@
+package onlytrade.app.viewmodel.login.repository.data
+
+object LoginConst {
+    const val JWT_TOKEN = "JWT_TOKEN"
+    const val JWT_USER = "JWT_USER"
+}

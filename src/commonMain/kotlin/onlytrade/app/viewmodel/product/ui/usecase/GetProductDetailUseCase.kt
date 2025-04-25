22638c27ceb13a80f8186557db08de645b8afb46
Mobin Munir <mmobinbutt@gmail.com>
2025-04-25 06:04:27 +0500

    Completed GetProductDetailUseCase impl.

--- src/commonMain/kotlin/onlytrade/app/viewmodel/product/ui/usecase/GetProductDetailUseCase.kt
+++ src/commonMain/kotlin/onlytrade/app/viewmodel/product/ui/usecase/GetProductDetailUseCase.kt
@@ -1,9 +1,15 @@
 package onlytrade.app.viewmodel.product.ui.usecase
 
+import kotlinx.coroutines.withContext
+import onlytrade.app.IODispatcher
 import onlytrade.app.viewmodel.product.repository.ProductRepository
 
 class GetProductDetailUseCase(private val productRepository: ProductRepository) {
-    suspend operator fun invoke(productId: Int) {
-        productRepository.g
+    suspend operator fun invoke(productId: Long) = withContext(IODispatcher) {
+        /**
+         * This call will always succeed because product detail can't happen or accessed without product.
+         * so result will always be returned.
+         */
+        productRepository.getProduct(productId = productId)
     }
 }

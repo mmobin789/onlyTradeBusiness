618e239b70c30b004e6a589a84abfb9829c36f03
Mobin Munir <mmobinbutt@gmail.com>
2025-05-01 02:09:56 +0500

    Fixed GetProductsApi url formation.

--- src/commonMain/kotlin/onlytrade/app/viewmodel/product/repository/data/remote/api/GetProductsApi.kt
+++ src/commonMain/kotlin/onlytrade/app/viewmodel/product/repository/data/remote/api/GetProductsApi.kt
@@ -5,6 +5,7 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.ResponseException
 import io.ktor.client.request.get
+import io.ktor.http.URLBuilder
 import io.ktor.http.parameters
 import onlytrade.app.viewmodel.product.repository.data.remote.response.GetProductsResponse
 
@@ -14,7 +15,13 @@ import onlytrade.app.viewmodel.product.repository.data.remote.response.GetProduc
 class GetProductsApi(private val client: HttpClient) {
 
     suspend fun getProducts(pageNo: Int, pageSize: Int, userId: Long? = null) = try {
-        client.get("https://onlytrade.co/products") {
+        val url = URLBuilder("https://onlytrade.co/products").apply {
+            parameters.append("size", pageSize.toString())
+            parameters.append("page", pageNo.toString())
+            userId?.let { parameters.append("uid", it.toString()) }
+        }.toString()
+
+        client.get(url) {
             parameters {
                 append("size", pageSize.toString())
                 append("page", pageNo.toString())

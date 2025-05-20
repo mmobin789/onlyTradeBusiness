f8f70fac9e2101d07257411b6646fcdea10d8e76
Mobin Munir <mmobinbutt@gmail.com>
2025-05-21 04:39:05 +0500

    Added pretty printing to json config.

--- src/commonMain/kotlin/onlytrade/app/di/OTBusinessModule.kt
+++ src/commonMain/kotlin/onlytrade/app/di/OTBusinessModule.kt
@@ -11,7 +11,6 @@ import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
-import io.ktor.serialization.kotlinx.json.DefaultJson
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import onlytrade.app.AppConfig
@@ -52,7 +51,7 @@ object OTBusinessModule {
                         socketTimeoutMillis = 120000   // 120 seconds
                     }
                     install(ContentNegotiation) {
-                        json(Json(DefaultJson) {
+                        json(Json {
                             ignoreUnknownKeys = true
                             prettyPrint = true
                         })

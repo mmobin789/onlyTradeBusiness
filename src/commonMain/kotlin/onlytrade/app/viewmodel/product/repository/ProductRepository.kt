d3345b53d93657366c01491ef45eab8b0e6d8ba1
Mobin Munir <mmobinbutt@gmail.com>
2025-06-02 10:15:29 +0500

    Need to test product listing and refresh my trades on offer complete.

--- src/commonMain/kotlin/onlytrade/app/viewmodel/product/repository/ProductRepository.kt
+++ src/commonMain/kotlin/onlytrade/app/viewmodel/product/repository/ProductRepository.kt
@@ -63,59 +63,75 @@ class ProductRepository(
         val productUpdateDateTime = Instant.parse(this)
         val now = Clock.System.now()
         val minutesDiff = productUpdateDateTime.until(now, DateTimeUnit.MINUTE)
-        val updateRequired = minutesDiff >= 1  // 2 minutes //todo need to update server sync time.
+        val updateRequired = minutesDiff >= 1  // 1 minute //todo need to update server sync time.
 
         if (updateRequired) {
             getProductsApi(pageNo, pageSize, userId)
-        }
+        } else {
+            val offset = if (pageNo > 1) {    // 2..20..3..40..4..60
+                ((pageSize * pageNo) - pageSize).toLong()
+            } else 0
 
-        val offset = if (pageNo > 1) {    // 2..20..3..40..4..60
-            ((pageSize * pageNo) - pageSize).toLong()
-        } else 0
+            val localProducts = if (userId != null) dao.selectUsersPaged(
+                userId.toLong(), pageSize.toLong(), offset
+            )
+            else dao.selectPaged(pageSize.toLong(), offset)
 
-        val localProducts = if (userId != null) dao.selectUsersPaged(
-            userId.toLong(), traded = false, pageSize.toLong(), offset
-        )
-        else dao.selectPaged(traded = false, pageSize.toLong(), offset)
+            return GetProductsResponse().run {
+                val localProductList = dao.transactionWithResult {
+                    localProducts.executeAsList().map(::toProduct)
+                }
+                if (localProductList.isEmpty()) {
+                    getProductsApi(pageNo, pageSize, userId)
+                } else {
+                    copy(
+                        statusCode = HttpStatusCode.PartialContent.value,
+                        products = localProductList
+                    )
+                }
 
-        return GetProductsResponse().run {
-            val localProductList = dao.transactionWithResult {
-                localProducts.executeAsList().map(::toProduct)
             }
-            if (localProductList.isEmpty()) {
-                getProductsApi(pageNo, pageSize, userId)
-            } else {
-                copy(
-                    statusCode = HttpStatusCode.PartialContent.value, products = localProductList
-                )
-            }
-
         }
     } ?: getProductsApi(pageNo, pageSize, userId)
 
 
-    private suspend fun getProductsApi(pageNo: Int, pageSize: Int, userId: Long? = null) =
-        getProductsApi.getProducts(pageNo, pageSize, userId).also {
+    private suspend fun getProductsApi(
+        pageNo: Int,
+        pageSize: Int,
+        userId: Long? = null
+    ): GetProductsResponse {
+        deleteProductsAndOffers(pageNo, pageSize, userId)
+        return getProductsApi.getProducts(pageNo, pageSize, userId).also {
             val products = it.products
-            if (products.isNullOrEmpty()) {
-                deleteProductsAndOffers()
-            } else {
+
+            if (!products.isNullOrEmpty()) {
                 localPrefs.putString(
                     productsLastUpdatedAt, Clock.System.now().toString()
                 )
                 addProductsAndOffers(products)
             }
         }
+    }
+
 
     /**
      * method to delete all products and offers from local db.
      * Do not call this from main thread.
      * Blocking synchronous operation.
      */
-    private fun deleteProductsAndOffers() = onlyTradeDB.transaction {
-        dao.deleteAll()
-        offerDao.deleteAll()
-    }
+    private fun deleteProductsAndOffers(pageNo: Int, pageSize: Int, userId: Long? = null) =
+        onlyTradeDB.transaction {
+
+            val offset = if (pageNo > 1) {    // 2..20..3..40..4..60
+                ((pageSize * pageNo) - pageSize).toLong()
+            } else 0
+            if (userId != null)
+                dao.deleteUserPaged(userId, pageSize.toLong(), offset)
+            else
+                dao.deletePaged(pageSize.toLong(), offset)
+
+            offerDao.deleteAll()
+        }
 
 
     /**
